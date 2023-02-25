8eb1ec0b5a4b97cd49d95e4310dca8ddca8fc39e
yasser00111m <yassertkj@gmail.com>
2023-02-25 08:24:48 +0800

    upload ke 2

--- admin/spp1.php
+++ admin/spp1.php
@@ -57,7 +57,7 @@ if($_SESSION['level']!='admin'){
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="admin.php">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
@@ -78,15 +78,15 @@ if($_SESSION['level']!='admin'){
 
       <!-- Nav Item - Charts -->
       <li class="nav-item">
-        <a class="nav-link" href="charts.html">
+        <a class="nav-link" href="admin.php">
           <i class="fas fa-fw fa-user"></i>
           <span>administrator</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="spp1.php">
+      <!-- <li class="nav-item">
+        <a class="nav-link" href="admin.php">
           <i class="fas fa-fw fa-money-bill"></i>
           <span>spp </span></a>
-      </li>
+      </li> -->
       <li class="nav-item">
         <a class="nav-link" href="charts.html">
           <i class="fas fa-fw fa-landmark"></i>
@@ -206,90 +206,41 @@ if($_SESSION['level']!='admin'){
 
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> -->
-          </div>
+            <h1 class="h3 mb-0 text-gray-800">DATA SPP</h1>
+            <a href="tambah-spp.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i> Tambah SPP</a>
+        </div>
+        <table class="table table">
+            <tr>
+                <th>No.</th>
+                <th>Tahun</th>
+                <th>Nominal</th>
+                <th>edit</th>
+                <th>Hapus</th>
+            </tr>
+            <?php 
+            include'../koneksi.php';
+            $no = 1;
+            $sql ="SELECT*FROM tb_spp ORDER BY id_spp DESC";
+            $query = mysqli_query($koneksi, $sql);
+            foreach($query as $data){  ?>
+            <tr>
+                <td><?= $no++ ?></td>
+                <td><?= $data['tahun'] ?></td>
+                <td><?= $data['nominal']?></td>
+                <td>
+                    <a href="?url=edit-spp&id_spp= <?=$data['id_spp'] ?>" class="btn btn-warning">Edit</a>
+                </td>
+                <td>
+                <a onclick="return confirm('Apakah anda yakin ingin Hapus Data? ')" href="?url=hapus-spp&id_spp= <?=$data['id_spp'] ?>" class="btn btn-danger">Hapus</a>
+
+                </td>
+            </tr>
+            <?php } ?>
+            
+        </table>
 
           <!-- Content Row -->
           <div class="row">
-
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
-                      <div class="row no-gutters align-items-center">
-                        <div class="col-auto">
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                        </div>
-                        <div class="col">
-                          <div class="progress progress-sm mr-2">
-                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <!-- Pending Requests Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-comments fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
 
           <!-- Content Row -->
 
